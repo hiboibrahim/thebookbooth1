1e4d48e4ac77cdc13ce73531233ae8a064d75104
hibo ibrahim <hibo.ibrahim@gmail.com>
2023-11-30 16:06:39 +0000

    add footer and socials

--- myproject/settings.py
+++ myproject/settings.py
@@ -157,8 +157,9 @@ ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
